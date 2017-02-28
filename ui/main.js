787e3ef924a400695ba0dac24a989dcfb0cf4879
aquariussaigon <saigonaquarius@gmail.com>
2017-02-28 21:02:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 // move the image
-var img = document.getElementBYId('img');
+var img = document.getElementBYId('ankit');
 img.onclick = function () {
  img.stylemarginRight = '100px';  
 };
